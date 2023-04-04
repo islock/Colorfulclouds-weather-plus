eea6ddee460a675e6f49ca9470458f095b38a48c
dscao <dscao.com@gmail.com>
2023-04-05 00:31:55 +0800

    change the js file from unpkg to local

--- custom_components/colorfulclouds/local/colorfulclouds-weather-card/colorfulclouds-weather-card.js
+++ custom_components/colorfulclouds/local/colorfulclouds-weather-card/colorfulclouds-weather-card.js
@@ -6,7 +6,7 @@ import {
 LitElement,
 html,
 css
-} from "https://unpkg.com/lit-element@2.0.1/lit-element.js?module";
+} from "./lit-element@2.0.1/lit-element.js?module";
 
 const includeDomains = ["weather"];
 const windDirections = [
